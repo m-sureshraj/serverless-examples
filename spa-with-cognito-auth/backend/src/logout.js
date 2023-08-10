e95283fc5fe2c90a18f2781c0aad29b7a21641af
Sureshraj <m.s.suresh100@gmail.com>
2023-08-10 21:22:54 +0200

    feat(cognito-auth-BE): initial BE implementation (#5)
    * feat(cognito-auth-BE): initial BE implementation

---
+++ spa-with-cognito-auth/backend/src/logout.js
@@ -0,0 +1,45 @@
+import {
+  CognitoIdentityProviderClient,
+  GlobalSignOutCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
+import { getAccessTokenFromReqContext } from './lib/util.js';
+
+const client = new CognitoIdentityProviderClient({});
+
+export const handler = async event => {
+  const accessToken = getAccessTokenFromReqContext(event);
+
+  const command = new GlobalSignOutCommand({
+    AccessToken: accessToken,
+  });
+
+  try {
+    await client.send(command);
+    console.info(
+      'Invalidated all identity, access and refresh tokens that Amazon Cognito has issued to a user'
+    );
+  } catch (error) {
+    console.error(
+      'An error occurred while trying to invalidate the identity, access and refresh tokens',
+      error
+    );
+  }
+
+  // clear the accessToken cookie regardless of the previous actions' outcome so the user will be signed out from the application.
+  const cookieOptions = [
+    'accessToken=',
+    'HttpOnly',
+    'SameSite=Strict',
+    'Secure',
+    `Expires=${new Date(1)}`, // A random past date; 1 second after 1/1/1970
+  ];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+    headers: {
+      'content-type': 'application/json',
+      'Set-Cookie': `${cookieOptions.join(';')}`,
+    },
+  };
+};
---
+++ spa-with-cognito-auth/backend/src/user-info.js
@@ -0,0 +1,33 @@
+import { getAccessTokenFromReqContext } from './lib/util.js';
+import { withErrorHandler, HttpError } from './lib/http-error';
+
+const cognitoDomain = process.env.COGNITO_DOMAIN;
+const userInfoUrl = `${cognitoDomain}/oauth2/userInfo`;
+
+export const getUserInfo = async event => {
+  const accessToken = getAccessTokenFromReqContext(event);
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const response = await fetch(userInfoUrl, {
+    method: 'GET',
+    headers: headers,
+  });
+  const responseBody = await response.json();
+
+  if (!response.ok) {
+    console.error('An error occurred while fetching the user information', responseBody);
+    throw new HttpError(responseBody.error_description, response.status);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+};
+
+export const handler = withErrorHandler(getUserInfo);
