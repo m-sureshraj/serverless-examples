f2e18c47535c6ffe070a4acc452404b1ca80920f
m-sureshraj <m.s.suresh100@gmail.com>
2023-09-17 20:41:29 +0200

    feat: layout design

--- spa-with-cognito-auth/frontend/src/components/Auth.tsx
+++ spa-with-cognito-auth/frontend/src/components/Auth.tsx
@@ -1,16 +1,36 @@
-import { Button, Flex, Text, Title, Box } from '@mantine/core';
+import { Button, Flex, Text, Title, Box, Notification } from '@mantine/core';
+import { useSearchParams } from 'react-router-dom';
 
 import { API_ENDPOINT } from '../config.ts';
+import { IconX } from '@tabler/icons-react';
 
 export function Auth() {
+  const [searchParams] = useSearchParams();
+  const isLoginFailed = searchParams.get('error') === 'auth_failed';
+
   return (
-    <Flex direction="column" align="center" mt="xl">
+    <Flex direction="column" align="center" mt={80}>
       <Title order={1} mb="sm">
         Authentication Required
       </Title>
+
       <Text fz="xl" mb="xl">
         Please log in to access this page
       </Text>
+
+      {isLoginFailed && (
+        <Notification
+          icon={<IconX size="1.1rem" />}
+          color="red"
+          withBorder
+          mb={40}
+          mt={20}
+          p="md"
+          withCloseButton={false}
+        >
+          An error occurred while logging you in. Please try again!
+        </Notification>
+      )}
 
       <Flex gap={10}>
         <Box w={120}>
--- spa-with-cognito-auth/frontend/src/components/index.ts
+++ spa-with-cognito-auth/frontend/src/components/index.ts
@@ -1,2 +1,4 @@
 export * from './ProtectedRoute.tsx';
 export * from './Header.tsx';
+export * from './PageLoader.tsx';
+export * from './Auth.tsx';
